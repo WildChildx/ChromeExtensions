bf94a9e4a29d208de63817d2ee923975e7da1d9a
Shree Chatane <58407535+WildChildx@users.noreply.github.com>
2023-09-10 16:10:19 +0530

    added a readme to readtime ext

---
+++ ReadingTime/README.md
@@ -0,0 +1,4 @@
+# Reading time calculator
+![Logo](./images/readTimeImage.avif)
+
+Inserts the timer below to give estimated readtime
--- ReadingTime/scripts/content.js
+++ ReadingTime/scripts/content.js
@@ -7,10 +7,14 @@ if (article) {
     const wordCount = [...words].length;
     const readingTime = Math.round(wordCount / 200);
     const badge = document.createElement('p');
+    //adding class to the tag
     badge.classList.add('color-secondary-text', 'type--caption');
     badge.textContent = `⏱️ ${readingTime} min read`;
+    //accessing heading tag
     const heading = article.querySelector('h1');
+    //accessing the date tag
     const date = article.querySelector('time')?.parentNode;
+    //placing the badge object after date / heading tag whatever is present in document
     (date ?? heading).insertAdjacentElement('afterend', badge);
 
 }
